dc2063d5e1c8a50ac8582cf8bcd3f737af2df752
Daniil <daniil.razgulin@mail.ru>
2021-09-11 09:47:31 +0300

    1v 3_hw

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,15 +1,17 @@
+'use strict';
 function compareArrays(arr1, arr2) {
   let result;
-
-  // Ваш код
-
-  return result; // boolean
+  // функция сравнения двух массивов
+  // boolean
+  result = (arr1.length === arr2.length && arr1.every((item, index) => item === arr2[index]));
+  return result;
 }
 
 function advancedFilter(arr) {
   let resultArr;
-
   // Ваш код
 
+  resultArr = arr.filter(item => item > 0 && item % 3 === 0).map(item => item * 10);
   return resultArr; // array
 }
+console.log(advancedFilter([-10, -21, 12, 123]));
