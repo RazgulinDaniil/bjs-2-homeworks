9caad920cee05ac9c1cfad271398b306c9bba7e1
Daniil <daniil.razgulin@mail.ru>
2021-09-16 23:42:51 +0300

    first commit

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -1,10 +1,39 @@
 function Student(name, gender, age) {
-    // Ваш код
-
+    this.name = name;
+    this.gender = gender;
+    this.age = age;
 }
+
+let student1 = new Student("Oleg", "male", 12);
+let student2 = new Student("Masha", "female", 11);
 
 Student.prototype.setSubject = function (subjectName) {
-  //ваш код
+  this.subject = subjectName;
 }
 
-// ваш код для остальных методов
+Student.prototype.addMark = function (mark) {
+  if(this.marks === undefined) {
+    this.marks = [];
+  } 
+  this.marks.push(mark);
+}
+
+Student.prototype.addMarks = function (...marks) {
+  if(this.marks === undefined) {
+    this.marks = marks;
+  } else {
+    this.marks = this.marks.concat(marks);
+  }
+}
+
+Student.prototype.getAverage = function () {
+  let sum = 0;
+  this.marks.forEach(item => sum += item);
+  return sum/this.marks.length;
+}
+
+Student.prototype.exclude = function (reason) {
+  delete this.subject;
+  delete this.marks;
+  this.excluded = reason;
+}
