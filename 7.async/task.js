13597704e55eee2c1fe0639c5e64e50a859c6db9
Daniil <daniil.razgulin@mail.ru>
2021-10-04 22:49:35 +0300

    1.0v

--- 7.async/task.js
+++ 7.async/task.js
@@ -1,3 +1,4 @@
+'use strict';
 class AlarmClock {
     constructor() {
         this.alarmcollection = [];
@@ -5,9 +6,9 @@ class AlarmClock {
     }
     addClock(time, callback, idTimer) {
         if(idTimer === undefined) {
-            throw new Error("error text");
+            throw new Error("Невозможно идентифицировать будильник. Праметр id не передан.");
         } else if(this.alarmcollection.some((item, idx) => item.id === idTimer)) {
-            return console.error('Данный ID уже существует');
+            return console.error("Будильник с таким id уже существует");
         }
         
         this.alarmcollection.push({
@@ -18,17 +19,27 @@ class AlarmClock {
     }
 
     removeClock(idTimer) {
+        // if(this.alarmcollection.findIndex((item, idx) => item.id === idTimer) != -1) {
+        //     this.alarmcollection = this.alarmcollection.filter((item, id) => item.id != idTimer);
+        //     return true;
+        // } else {
+        //     console.error(`Таймера с таким id = ${idTimer} не существует`));
+        //     return false;
+        // }
+        let valueClock = this.alarmcollection.length;
         let alarmIdx = this.alarmcollection.findIndex((item, idx) => item.id === idTimer);
-
+        
         if(alarmIdx === -1) {
-            console.log(`Таймера с таким id = ${idTimer} не существует`);
+            console.error(`Таймера с таким id = ${idTimer} не существует`);
+            return false;
         } else {
             this.alarmcollection.splice(alarmIdx, 1);
+            return true;
         }
     }
 
 
-    getCurrentFormattedTime(uppMinutes = 0) {
+    getCurrentFormattedTime(upMin = 0) {
         let date = new Date();
         let hours = date.getHours();
 
@@ -36,8 +47,10 @@ class AlarmClock {
             hours = '0' + hours;
         }
 
-        let minutes = date.getMinutes + uppMinutes;
-
+        let minutes = date.getMinutes() + upMin;
+        if(minutes >= 60 ) {
+            minutes -= 60;
+        }
         if(minutes < 10) {
             minutes = '0' + minutes;
         }
@@ -45,19 +58,64 @@ class AlarmClock {
     }
 
     start() {
-        const checkClock = () => {
-            this.alarmcollection.some((item,idx) => {
-                if(item.time === this.getCurrentFormattedTime()) {
-                    return item.time;
-                }
-            }    
-        };
-        let date = new Date();
+       const checkClock = (timer) => {
+           if(timer.time === this.getCurrentFormattedTime()){
+               timer.callback();
+           }
+       };
+
+       if(this.timerId === null) {
+           this.timerId = setInterval(() => {
+               this.alarmcollection.forEach(checkClock);
+           }, 10000);
+       }
         
     }
 
+    stop() {
+        if(this.timerId === null) {
+            throw new Error("Таймер интервала не задан!");
+        }
+        clearInterval(this.timerId);
+        this.timerId = null;
+    }
+    printAlarms() {
+        this.alarmcollection.forEach(item => console.log(`будильник номер ${item.id} заведен на  ${item.time}`));
+    }
 
+    clearAlarms() {
+        clearInterval(this.timerId);
 
+        this.alarmcollection.forEach((item, idx) => this.alarmcollection.splice(idx,1));
+    }
 
 }
+
+function testCase (obj) {
+    //создаем таймер
+    obj = new AlarmClock();
+    //добавляем будильники
+    obj.addClock(obj.getCurrentFormattedTime(),() => console.log("Пора вставать"), 1);
+    obj.addClock(obj.getCurrentFormattedTime(1),() => {console.log("Пора вставать!!!"); obj.removeClock(2)}, 2);
+
+    obj.addClock(obj.getCurrentFormattedTime(2),() => {console.log("Вставай а то проспишь!");
+    obj.clearAlarms();obj.printAlarms()}, 3);
+    //ошибка с одинаковым идентификатором
+    // obj.addClock(obj.getCurrentFormattedTime(5),() => console.log("Вставай а то проспишь!"), 1);
+    //ошибка с незаданным id
+    // obj.addClock(obj.getCurrentFormattedTime(1),() => console.log("Иди умываться!"));
+    //3 будильника
+    obj.printAlarms();
+
+    //1 убрали
+    obj.removeClock(3);
+
+    // 2 будильника
+    obj.printAlarms();
+    // obj.start();
+}
+
+
+
+
 
